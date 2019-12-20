5389295bdd6cc5968600496c201b1cf374692147
cypress777 <chenypress@gmail.com>
2019-12-20 15:21:13 +0000

    implement by flask frame

--- ascension_server.py
+++ ascension_server.py
@@ -1,126 +1,119 @@
 #!/usr/bin/env python3
 
-import os
-import sys
-import threading
-from socketserver import ThreadingMixIn
-from http.server import HTTPServer, BaseHTTPRequestHandler
-from urllib.parse import urlparse, parse_qs
+from flask import Flask, render_template, request, url_for, redirect, flash
+app = Flask(__name__)
 
-from util_forms import fail_form, login_form, signup_form
-
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.pool import StaticPool
-
+from database_utils import DatabaseException, init_database
 from database_setup import Base, DatabaseName, UserInfo
 
-from utils import infoLogger, errorLogger, debugLogger
+from utils import infoLogger, errorLogger, debugLogger, ErrorCodes
 
-ErrorCodes = {404 : 'page not found/n\n'}
-
-class DatabaseException(Exception): pass
 DatabaseSession = None
 
-class IndexHandler(BaseHTTPRequestHandler):
-  global DatabaseSession, ErrorCodes
-
-  def do_GET(self):
-    debugLogger(self.path)
-    try:
-      if self.path.endswith('/'):
-        self.send_response(303)
-        self.send_header('Location', 'http://voidcypherplay.com:80')
-        self.end_headers()
-      elif self.path.endswith('/login'):
-        self.send_response(200)
-        self.send_header('Content-type', 'text/html')
-        self.end_headers()
-  
-        f = open(os.curdir + os.sep + 'login.html')
-        self.wfile.write(f.read().encode())
-        f.close()
-      else:
-        self.send_error(404, ErrorCodes[404])
-    except IOError:
-      self.send_error(404, ErrorCodes[404])
- 
-  def do_POST(self):
-    try:
-      length = int(self.headers.get('Content-length', 0))
-  
-      data = self.rfile.read(length).decode()
-  
-      username = parse_qs(data)['username'][0]
-      password = parse_qs(data)['pw'][0]
-      is_login = parse_qs(data)['islogin'][0]
-
-      userinfo = DatabaseSession.query(UserInfo).filter_by(name=username).first()
-
-      user_existed = (userinfo != None)
-      password_matched = (user_existed and userinfo.password == password)
-
-      self.send_response(200)
-      self.send_header('Content-type', 'text/html')
-      self.end_headers()
- 
-      if is_login == 'true':
-        if not user_existed:
-          self.wfile.write(fail_form.format('user not exist').encode())
-        elif not password_matched:
-          self.wfile.write(fail_form.format('wrong password').encode())
-        else:
-          self.wfile.write(login_form.format(username).encode())
-      else:
-        if user_existed:
-          self.wfile.write(fail_form.format('user existed, please try another').encode())
-        else:
-          new_user = UserInfo(name=username, password=password)
-          DatabaseSession.add(new_user)
-          DatabaseSession.commit()
-
-          self.wfile.write(signup_form.format(username).encode())
-    except:
-      errorLogger('unknown error', sys.exc_info())
-      try:
-        self.send_response(303)
-        self.send_header('Location', 'http://voidcypherplay.com:80')
-        self.end_headers()
-      except IOError:
-        self.send_error(404, ErrorCodes[404])
-
-class ThreadHTTPServer(ThreadingMixIn, HTTPServer):
-  pass
-
-def initDataBase():
-  global DatabaseSession
+def query_userinfo(username):
   try:
-    engine = create_engine('sqlite:///{}'.format(DatabaseName),
-      connect_args={'check_same_thread': False},
-      poolclass=StaticPool, echo=True)
-    Base.metadata.bind = engine
-    DBSession = sessionmaker(bind = engine)
-    DatabaseSession = DBSession()
-    infoLogger('database inited')
+    return DatabaseSession.query(UserInfo).filter_by(name=username).first()
   except:
-    raise DatabaseException('during database init')
+    raise DatabaseException('during query user\'s info')
+ 
 
+def has_username(username):
+  global DatabaseSessioin
+  try:
+    userinfo = query_userinfo(username)
+    return (userinfo != None)
+  except:
+    raise
+
+def is_valid_log_in(username, password):
+  global DatabaseSessioin
+
+  userinfo = None
+  try:
+    userinfo = query_userinfo(username)
+  except:
+    raise
+
+  if userinfo == None or userinfo.password != password:
+    return False
+  return True
+
+def log_in_user(username):
+  return render_template('welcome.html', welcome_info='Welcome Back', username=username)
+
+def is_valid_sign_up(username, password):
+  global DatabaseSessioin
+
+  userinfo = None
+  try:
+    userinfo = query_userinfo(username)
+  except:
+    raise
+
+  if userinfo != None:
+    return False
+  return True
+
+def sign_up_user(username, password):
+  try:
+    new_user = UserInfo(name=username, password=password)
+    DatabaseSession.add(new_user)
+    DatabaseSession.commit()
+  except:
+    raise
+
+  return render_template('login.html')
+
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/en', methods=['GET', 'POST'])
+def Welcome():
+  return render_template('hello.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def Login():
+  error = None
+
+  if request.method == 'POST':
+    username = request.form['username']
+    password = request.form['password']
+
+    if is_valid_log_in(username, password):
+      return log_in_user(username)
+    else:
+      error = 'Invalid log in: Wrong username/password'
+    flash(error)
+
+  return render_template('login.html')
+
+@app.route('/signup', methods=['GET', 'POST'])
+def Signup():
+  error = None
+
+  if request.method == 'POST':
+    username = request.form['username']
+    password = request.form['password']
+
+    if is_valid_sign_up(username, password):
+      return sign_up_user(username, password)
+    else:
+      if has_username(username):
+        error = 'Invalid sign up: Username has been taken'
+      else:
+        error = 'Invalid password: password should be letters and numbers'
+      flash(error)
+
+  return render_template('signup.html')
 
 if __name__ == '__main__':
-  httpd = None
   try:
-    initDataBase()
-    server_addr = ('', 8000)
-    httpd = ThreadHTTPServer(server_addr, IndexHandler)
-    infoLogger('server running')
-    httpd.serve_forever()
+    DatabaseSession = init_database(DatabaseName, Base)
+    app.secret_key = 'super_secret_key'
+    app.debug = True
+    app.run(host = '0.0.0.0', port = 8000)
   except KeyboardInterrupt:
     errorLogger('^C closing server')
-  except DatabaseException as de:
-    print(de, sys.exc_info())
+  except DatabaseException as e:
+    errorLogger(e, sys.exc_info())
   finally:
-    if httpd != None:
-      httpd.socket.close()
     if DatabaseSession != None:
       DatabaseSession.close()
-
