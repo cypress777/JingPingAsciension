a0ca0e4e32ce717b9a8ca2a6332224395b10c16a
cyp <cyp@wjj>
2020-01-02 19:48:41 +0800

    feature: use light color for titles after click

--- static/hello.js
+++ static/hello.js
@@ -1,7 +1,7 @@
 var helloApp = angular.module('helloApp', ['ngAnimate']);
 
 helloApp.controller('helloController', function() {
-    this.hideIntro=true;
-    this.hideRules=true;
-    this.hideTips=true;
+    this.hideIntro = true;
+    this.hideRules = true;
+    this.hideTips = true;
 });
