72e746e6707d9f930be958d394fc81ca622f0462
cyp <cyp@wjj>
2020-01-11 07:31:39 +0800

    format: use ng-repeat intead of for loop in hello page

--- static/hello.js
+++ static/hello.js
@@ -7,6 +7,16 @@ helloApp.controller('helloController', function($scope) {
                            "Tips are ..."]
     $scope.activeTitles = ["title-active-intro", "title-active-rules", "title-active-tips"];
     $scope.hideTitles = [true, true, true];
+    $scope.titleNum = $scope.titles.length;
+
+    $scope.range = function(min, max, step) {
+        step = step || 1;
+        var input = [];
+        for (var i = min; i <= max; i += step) {
+            input.push(i);
+        }
+        return input;
+    };
 
     $scope.getTitle = function(id) {
         return $scope.titles[id];
