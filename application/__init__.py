c5848033843516e095280267667248deaffdc5cf
cyp <cyp@wjj>
2020-01-13 13:22:32 +0800

    reformat: reorganize files into application

---
+++ application/__init__.py
@@ -0,0 +1,26 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+
+
+db = SQLAlchemy()
+login_manager = LoginManager()
+
+
+def create_app():
+    """Construct the core application."""
+    app = Flask(__name__, instance_relative_config=False)
+
+    db.init_app(app)
+    login_manager.init_app(app)
+
+    with app.app_context():
+        # from . import routes
+        # from . import auth
+
+        # app.register_blueprint(routes.main_bp)
+        # app.register_blueprint(auth.auth_bp)
+
+        db.create_all()
+
+        return app
