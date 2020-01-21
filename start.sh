5586f8bcbbc49e739ff1dc1ead5a48ba55df274d
EC2 Default User <ec2-user@ip-10-0-0-129.ap-southeast-2.compute.internal>
2020-01-21 04:08:39 +0000

    minor: stop listening from public

--- start.sh
+++ start.sh
@@ -3,4 +3,4 @@ export FLASK_DEBUG=false
 export APP_CONFIG_FILE=config.py
 export SQLALCHEMY_TRACK_MODIFICATIONS=false
 export LC_ALL='en_US.utf-8'
-python3 -m flask run --port=8000 --host=0.0.0.0
+python3 -m flask run --port=8000
