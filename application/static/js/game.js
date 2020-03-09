c26998912cd6109f30ae3aaf382f183cd96d0ad2
cyp <cyp@wjj>
2020-03-09 17:28:14 +0800

    add class

--- application/static/js/game.js
+++ application/static/js/game.js
@@ -7,37 +7,75 @@ app.config(['$interpolateProvider', function($interpolateProvider) {
     $interpolateProvider.endSymbol('a}');
 }]);
 
-class card {
-    constructor(name, avail, id, description) {
-        this.id = id;
-        this.name = name;
-        this.avail = avail;
-        this.description = description;
-    }
-}
-
-class userState {
-    constructor() {
-        this.readyDrawCard = true;
-    }
-}
-
 function getRandInt(min, max) {
     len = max - min;
     return min + Math.floor(len * Math.random());
 }
 
+class Card {
+    constructor(name, cost, effect, id, description)
+}
+
+class HeroCard extends Card {
+    constructor(name, cost, effect, faction, honor, rarity, avail, id, description) {
+        this.id = id;
+        this.name = name;
+        this.cost = cost;
+        this.effect = effect;
+        this.faction = faction;
+        this.honor = honor;
+        this.rarity = rarity;
+        this.avail = avail;
+        this.description = description;
+    }
+}
+
+class ConstructCard extends Card {
+    constructor(name, cost, effect, faction, honor, rarity, avail, id, description) {
+        this.id = id;
+        this.name = name;
+        this.cost = cost;
+        this.effect = effect;
+        this.faction = faction;
+        this.honor = honor;
+        this.rarity = rarity;
+        this.avail = avail;
+        this.description = description;
+    }
+}
+
+class MonsterCard extends Card {
+    constructor(name, cost, effect, reward, rarity, avail, id, description) {
+        this.id = id;
+        this.name = name;
+        this.cost = cost;
+        this.effect = effect;
+        this.reward = reward;
+        this.rarity = rarity;
+        this.avail = avail;
+        this.description = description;
+    }
+}
+
+class UserState {
+    constructor() {
+        this.readyDrawCard = false;
+        this.honorPoints = 0;
+        this.onHand = null;
+        this.openHand = null;
+        this.startingHand = null;
+        this.personalDeck = null;
+        this.discardPile = null;
+        IntersectionObserver(0)
+    }
+}
+
 function gameSetting($scope, $document) {
     $scope.user = null;
-    $scope.centerCards = [
-        {name: "king", avail: true, description: "is king", id: 0},
-        {name: "queen", avail: true, description: "is queen", id: 1},
-        {name: "jocker", avail: true, description: "is jocker", id: 2},
-        {name: "cultist", avail: true, description: "is jocker", id: 3},
-        {name: "cultist", avail: true, description: "is jocker", id: 4},
-    ];
-    $scope.centerCardsNum = 3;
-    $scope.curCenterCards = [];
+    $scope.counter = null;
+    $scope.centerRow = null;
+    $scope.centerDeck = null;
+    $scope.voidDeck = null;
 
     $scope.shuffleCards = function(cards, num) {
         new_cards = [];
@@ -66,17 +104,9 @@ function gameSetting($scope, $document) {
         }
     };
 
-    $scope.getName = function (card) {
-        if (card.avail) return card.name;
-        else return "";
-    }
-
-    $scope.getDescription = function (card) {
-        if (card.avail) return card.description;
-        else return "";
-    }
-
     $document.ready(function() {
-        $scope.user = new userState();
+        $scope.user = new UserState();
+        $scope.counter = new UserState();
+
     });
 };
